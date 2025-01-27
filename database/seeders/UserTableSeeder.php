fe8d39d24a078cf7c6688f7f6702a860b01e581f
riyanari <riyanari611@gmail.com>
2025-01-27 15:52:18 +0700

    feat: setup spatie

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mymovie.test',
+            'password' => bcrypt('12345678')
+        ]);
+        $admin->assignRole('admin');
+    }
+}
